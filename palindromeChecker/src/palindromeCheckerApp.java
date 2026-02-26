e76d8e15f4c0d66305f31d77cd4e8c1c0e281e2e
ParvGupta37 <parvgupta348@gmail.com>
2026-02-26 14:51:37 +0530

    Palindrome Checker

--- palindromeChecker/src/palindromeCheckerApp.java
+++ palindromeChecker/src/palindromeCheckerApp.java
@@ -1,2 +1,21 @@
+import java.util.*;
+
 public class palindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
 }
